6b0a5339dacd2d301caf285187f04e177ca31e21
Matheus Marabesi <matheus.marabesi@gmail.com>
2022-01-22 19:01:35 +0100

    chore: creating post

--- src/main/kotlin/Post.kt
+++ src/main/kotlin/Post.kt
@@ -1,13 +1,13 @@
 import picocli.CommandLine
 import java.util.concurrent.Callable
 
-@CommandLine.Command(name = "post", helpCommand = true)
-class Post: Callable<Int> {
+@CommandLine.Command(name = "post", mixinStandardHelpOptions = true)
+class Post: Callable<String> {
     @CommandLine.Option(names = ["-c"], description = ["Creates a post"])
     lateinit var text: String
 
-    override fun call(): Int {
+    override fun call(): String {
         println("this is my first post")
-        return 0
+        return "Post has been created"
     }
 }
--- src/test/kotlin/unit/PostCreatorTest.kt
+++ src/test/kotlin/unit/PostCreatorTest.kt
@@ -2,25 +2,17 @@ package unit
 
 import Post
 import org.junit.jupiter.api.Test
-import picocli.CommandLine
-import java.io.PrintWriter
-import java.io.StringWriter
 import kotlin.test.assertEquals
 
 class PostCreatorTest {
 
     @Test
     fun `should create post with the desired text`() {
-        val cmd = CommandLine(Post())
-
-        val sw = StringWriter()
-        cmd.out = PrintWriter(sw)
-        cmd.err = PrintWriter(sw)
-
-        cmd.execute()
+        val post = Post()
+        post.text = "my text"
 
         assertEquals("""
             Post has been created
-        """.trimIndent(), sw.toString())
+        """.trimIndent(), post.call().toString())
     }
 }

5efd8de02c0ef0980c181ef8dca8eddd6b4e19aa
Matheus Marabesi <matheus.marabesi@gmail.com>
2022-04-16 12:01:56 +0200

    chore: setting up create post with cucumber

---
+++ src/test/kotlin/acceptance/CreatePost.kt
@@ -0,0 +1,15 @@
+package acceptance
+
+import io.cucumber.java8.En
+import io.cucumber.java8.PendingException
+
+class CreatePost: En {
+    init {
+        When(
+            "I create a post with the title {string}"
+        ) { _: String? -> throw PendingException() }
+        Then(
+            "Show successfull menssage {string}"
+        ) { _: String? -> throw PendingException() }
+    }
+}

cc1cee2d7565554896f4546adb5d31975ed6b89c
Matheus Marabesi <matheus.marabesi@gmail.com>
2022-04-17 12:55:10 +0200

    chore: replaced in memory factory by filesystem

--- src/main/kotlin/MyFactory.kt
+++ src/main/kotlin/MyFactory.kt
@@ -1,11 +1,11 @@
-import persistence.InMemoryRepository
+import persistence.FileSystemRepository
 import picocli.CommandLine
 
 class MyFactory: CommandLine.IFactory {
     override fun <K : Any?> create(cls: Class<K>?): K {
         if (cls != null) {
             if (cls == Post::class.java) {
-                return Post(InMemoryRepository()) as K
+                return Post(FileSystemRepository("data/social-production.csv")) as K
             }
         }
         return CommandLine.defaultFactory().create(cls)

21a0f48f806d8c990bbca67620b6838b4d0c770a
Matheus Marabesi <matheus.marabesi@gmail.com>
2022-05-29 17:12:41 +0200

    refactor: moved class setup to beforeEach

--- src/test/kotlin/unit/PostSchedulerTest.kt
+++ src/test/kotlin/unit/PostSchedulerTest.kt
@@ -1,6 +1,7 @@
 package unit
 
 import Scheduler
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import persistence.InMemoryRepository
 import picocli.CommandLine
@@ -10,27 +11,29 @@ import java.io.StringWriter
 import kotlin.test.assertEquals
 
 class PostSchedulerTest {
+    private lateinit var app: Scheduler
+    private lateinit var cmd: CommandLine
+    private lateinit var sw: StringWriter
+
+    @BeforeEach
+    fun setUp() {
+        app = Scheduler(InMemoryRepository())
+        cmd = CommandLine(app)
+
+        sw = StringWriter()
+
+        cmd.out = PrintWriter(sw)
+        cmd.err = PrintWriter(sw)
+    }
 
     @Test
     fun `should show friendly message when required fields are not provided`() {
-        val app = Scheduler(InMemoryRepository())
-        val cmd = CommandLine(app)
-
-        val sw = StringWriter()
-        cmd.out = PrintWriter(sw)
-
         cmd.execute()
         assertEquals("Missing required fields", sw.toString())
     }
 
     @Test
     fun `should show help message for schedule command`() {
-        val app = Scheduler(InMemoryRepository())
-        val cmd = CommandLine(app)
-
-        val sw = StringWriter()
-        cmd.out = PrintWriter(sw)
-
         cmd.execute("--help")
         assertEquals("""
             Usage: scheduler [-hV] [-d=<targetDate>] [-p=<postId>]
@@ -44,13 +47,6 @@ class PostSchedulerTest {
 
     @Test
     fun `should show message if post id does not exists`() {
-        val postsRepository = InMemoryRepository()
-        val app = Scheduler(postsRepository)
-        val cmd = CommandLine(app)
-
-        val sw = StringWriter()
-        cmd.out = PrintWriter(sw)
-
         cmd.execute("-p", "1", "-d", "2022-10-02 at 09:00 PM")
         assertEquals("Couldn't find post with id 1", sw.toString())
     }

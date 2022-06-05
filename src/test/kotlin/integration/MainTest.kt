33ea1d8a5862c40dae3a8a3233c20c89a3235791
Matheus Marabesi <matheus.marabesi@gmail.com>
2022-06-05 23:33:57 +0200

    refactor: moved post test under post file

--- src/test/kotlin/integration/MainTest.kt
+++ src/test/kotlin/integration/MainTest.kt
@@ -27,23 +27,4 @@ class MainTest {
         
         """.trimIndent(), sw.toString())
     }
-
-    @Test
-    fun `should show help message for post command`() {
-        val cmd = buildCommandLine()
-
-        val sw = StringWriter()
-        cmd.out = PrintWriter(sw)
-        cmd.err = PrintWriter(sw)
-
-        cmd.execute("post", "--help")
-        assertEquals("""
-            Usage: social post [-hlV] [-c=<text>]
-              -c=<text>       Creates a post
-              -h, --help      Show this help message and exit.
-              -l              List created posts
-              -V, --version   Print version information and exit.
-        
-        """.trimIndent(), sw.toString())
-    }
 }
--- src/test/kotlin/unit/PostCreatorTest.kt
+++ src/test/kotlin/unit/PostCreatorTest.kt
@@ -1,5 +1,6 @@
 package unit
 
+import buildCommandLine
 import cli.Post
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
@@ -7,9 +8,30 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 import persistence.InMemoryRepository
 import picocli.CommandLine
+import java.io.PrintWriter
+import java.io.StringWriter
 
 class PostCreatorTest {
     private val cmd = CommandLine(Post(InMemoryRepository(), MockedOutput()))
+
+    @Test
+    fun `should show help message for post command`() {
+        val cmd = buildCommandLine()
+
+        val sw = StringWriter()
+        cmd.out = PrintWriter(sw)
+        cmd.err = PrintWriter(sw)
+
+        cmd.execute("post", "--help")
+        assertEquals("""
+            Usage: social post [-hlV] [-c=<text>]
+              -c=<text>       Creates a post
+              -h, --help      Show this help message and exit.
+              -l              List created posts
+              -V, --version   Print version information and exit.
+        
+        """.trimIndent(), sw.toString())
+    }
 
     @Test
     fun `should show friendly message when no arguments is provided to post`() {

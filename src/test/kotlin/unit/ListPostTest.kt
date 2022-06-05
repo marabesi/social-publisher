6a5998e8868e4d7e91390c60af1ca3fcb20ff3ac
Matheus Marabesi <matheus.marabesi@gmail.com>
2022-06-05 10:56:54 +0200

    refactor(post-list): moved setup from each test to the class

--- src/test/kotlin/unit/ListPostTest.kt
+++ src/test/kotlin/unit/ListPostTest.kt
@@ -7,11 +7,10 @@ import picocli.CommandLine
 import kotlin.test.assertEquals
 
 class ListPostTest {
+    private val cmd = CommandLine(Post(InMemoryRepository()))
 
     @Test
     fun `should list post created`() {
-        val cmd = CommandLine(Post(InMemoryRepository()))
-
         cmd.execute("-c", "this is my first post")
         cmd.execute("-l")
 
@@ -24,9 +23,6 @@ class ListPostTest {
 
     @Test
     fun `should list posts created`() {
-        val repository = InMemoryRepository()
-        val cmd = CommandLine(Post(repository))
-
         cmd.execute("-c", "this is my first post")
         cmd.execute("-c", "this is my second post")
         cmd.execute("-l")
@@ -41,8 +37,6 @@ class ListPostTest {
 
     @Test
     fun `should list post with three dots if it is greater than 50 chars`() {
-        val cmd = CommandLine(Post(InMemoryRepository()))
-
         cmd.execute("-c", "caracters, our online editor can help you to improve word choice and writing style, and, optionally, help you to detect grammar mistakes and plagiarism. To check word count, simply 1")
         cmd.execute("-l")
 

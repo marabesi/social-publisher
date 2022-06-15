f85d686c95d910ba79193a3ca1ae31e7b9426783
Matheus Marabesi <matheus.marabesi@gmail.com>
2022-06-15 23:18:34 +0200

    chore: using configuration to load up repositories

--- src/main/kotlin/cli/CliFactory.kt
+++ src/main/kotlin/cli/CliFactory.kt
@@ -4,17 +4,25 @@ import persistence.FileSystemConfigurationRepository
 import persistence.FileSystemPostRepository
 import persistence.FileSystemSchedulerRepository
 import picocli.CommandLine
+import socialPosts.SocialConfiguration
 
 class CliFactory: CommandLine.IFactory {
     override fun <K : Any?> create(cls: Class<K>?): K {
         if (cls != null) {
-            val postsRepository = FileSystemPostRepository("data/social-production.csv")
+            val configuration = FileSystemConfigurationRepository()
+            val currentConfiguration: SocialConfiguration = try {
+                configuration.find()
+            } catch (_:MissingConfiguration) {
+                SocialConfiguration("production", "csv")
+            }
+
+            val postsRepository = FileSystemPostRepository("data/posts-${currentConfiguration.fileName}.csv")
 
             if (cls == Post::class.java) {
                 return Post(postsRepository, CliOutput()) as K
             }
             if (cls == Scheduler::class.java) {
-                val filePath = "data/scheduler-social-production.csv"
+                val filePath = "data/scheduler-${currentConfiguration.fileName}.csv"
                 val scheduler = FileSystemSchedulerRepository(filePath)
                 return Scheduler(postsRepository, scheduler, CliOutput()) as K
             }

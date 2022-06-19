a1ee261d935cfca03ee277b5d43962c32e1af84d
Matheus Marabesi <matheus.marabesi@gmail.com>
2022-06-19 20:00:01 +0200

    chore: removed unused exception

--- src/main/kotlin/application/socialnetwork/UnauthorizedAttemptToTweet.kt
+++
@@ -1,7 +0,0 @@
-package application.socialnetwork
-
-class UnauthorizedAttemptToTweet(
-    private val details: String
-): Throwable(
-    message = "Unauthorized attempt to send a tweet, verify your credentials: $details"
-)

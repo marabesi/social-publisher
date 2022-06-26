06c8a2c8ccb8fa0e98fed87b0debf46d41af2e81
Matheus Marabesi <matheus.marabesi@gmail.com>
2022-06-26 16:55:09 +0200

    --amend

--- src/main/kotlin/application/poster/Executor.kt
+++ src/main/kotlin/application/poster/Executor.kt
@@ -7,7 +7,6 @@ import java.time.Instant
 import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
 
-
 class Executor(
     private val schedulerRepository: SchedulerRepository,
     private val cliOutput: Output,
